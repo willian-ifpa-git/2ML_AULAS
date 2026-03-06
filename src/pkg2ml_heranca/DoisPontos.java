065f60c54eafc5950d75b17df94a8e759dfd7414
willianIFPA <willian.virgilio@ifpa.edu.br>
2026-03-06 09:11:14 -0300

    teste

---
+++ src/pkg2ml_heranca/DoisPontos.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkg2ml_heranca;
+
+public class DoisPontos {
+    
+    private float coordX;
+    private float coordY;
+    
+public DoisPontos(float coordX, float y){ // Método Construtor
+    this.coordX = coordX;
+    coordY = y;
+
+}
+
+public void setCoordX(float x){
+    this.coordX = x;
+}
+public float getCoordX(){
+    return coordX;
+
+}
+
+
+public void setCoordY(float y){
+    this.coordY = y;
+
+}
+
+public float getCoordY(){
+    return coordY;
+}
+    
+}
+
+
+
+
+
+
+
+
